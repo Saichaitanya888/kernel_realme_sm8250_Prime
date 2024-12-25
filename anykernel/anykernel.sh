1cb4e71c5d35bdb243ce135a1101f6307dd8cfd7
Saichaitanya888 <pothulashanmukhasaichaitanya@gmail.com>
2024-12-25 18:02:51 +0000

    defconfig: Changes required for RUI

--- anykernel/anykernel.sh
+++ anykernel/anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Realme GT Neo 2 / Realme X50 Pro B I G S H O T
+kernel.string=Realme GT Neo 2  B I G S H O T - P R I M E
 do.devicecheck=0
 do.modules=0
 do.systemless=1
