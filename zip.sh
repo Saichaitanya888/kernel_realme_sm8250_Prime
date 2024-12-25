e6f4e393f6ebe6c18bc666c5c4f6409465bc0de1
Saichaitanya888 <pothulashanmukhasaichaitanya@gmail.com>
2024-12-25 18:02:51 +0000

    defconfig: Changes required for RUI

--- zip.sh
+++ zip.sh
@@ -1,7 +1,6 @@
 cp out/arch/arm64/boot/Image ./anykernel/
 cp out/arch/arm64/boot/dtbo.img ./anykernel/
 
-
-cd anykernel && zip -r Bigshot-kernel-AOSP-$(date +"%d-%m-%Y"-%H%M).zip * && mv Bigshot-kernel-AOSP-$(date +"%d-%m-%Y-%H%M").zip ../out/
+cd anykernel && zip -r BigShot-Prime-kernel-$(date +"%d-%m-%Y"-%H%M).zip * && mv BigShot-Prime-kernel-$(date +"%d-%m-%Y-%H%M").zip ../out/
 
 cd ..
